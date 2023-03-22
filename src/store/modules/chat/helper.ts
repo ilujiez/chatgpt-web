ffc277f1b161cf73c302f3bf333a427174887210
ilujiez <zhenglujie@appledeMacBook-Pro-2.local>
2023-03-22 17:39:51 +0800

    update help.ts

--- src/store/modules/chat/helper.ts
+++ src/store/modules/chat/helper.ts
@@ -3,18 +3,13 @@ import { ss } from '@/utils/storage'
 const LOCAL_NAME = 'chatStorage'
 
 export function defaultState(): Chat.ChatState {
-<<<<<<< HEAD
   const uuid = 1
-  return { active: uuid, history: [{ uuid, title: 'New Chat', isEdit: false }], chat: [{ uuid, data: [] }] }
-=======
-  const uuid = 1002
   return {
     active: uuid,
     usingContext: true,
     history: [{ uuid, title: 'New Chat', isEdit: false }],
     chat: [{ uuid, data: [] }],
   }
->>>>>>> upstream/main
 }
 
 export function getLocalState(): Chat.ChatState {
